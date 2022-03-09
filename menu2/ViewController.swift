80bdfdf0bfea9865cb8c4ebcb0793adf75d81af8
Avinashg137 <avinashg.itpath@gmail.com>
2022-03-09 12:29:06 -0800

    update

--- menu2/ViewController.swift
+++ menu2/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var viewmenu: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet weak var menubtn: UIButton!
+    
+    @IBAction func menubtnpress(_ sender: Any) {
+        if (viewmenu.isHidden == true){
+            viewmenu.isHidden = false
+        }
+        else {
+            viewmenu.isHidden = true
+        }
+    }
 }
 
